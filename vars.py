fb26df347ff55555beaf18a1e78b5903ef2b849e
newkrishna983-lang <newkrishna983@gmail.com>
2026-05-21 10:57:22 +0530

    Change API_ID, API_HASH, and BOT_TOKEN
    Updated API credentials with new values.

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "2447366")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "5dghgfccc") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:A") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "34422904")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8582068316:AAE_TOrc7ccSqO9A5LTeDy50c5tRv6XKBXE") #Replace with your bot token
